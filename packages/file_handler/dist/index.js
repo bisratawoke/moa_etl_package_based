3e2e3aeaeba7d9942856f75d2fb4bc56e7283de2
bisratawoke <bisrat@360ground.com>
2024-02-12 16:20:05 +0300

    Subject Line, finisehed file handler

--- packages/file_handler/dist/index.js
+++ packages/file_handler/dist/index.js
@@ -3,20 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var multer = require("multer");
 var cors = require("cors");
+var path = require("path");
 var app = express();
 app.use(cors());
 app.use(express.static("./packages/file_handler/dist/uploads"));
+function replaceNonAlphabeticCharactersWithUnderscore(str) {
+    // Replace spaces, hyphens, and any non-alphabetic characters with underscores
+    return str.replace(/[^a-zA-Z]/g, "_");
+}
 // Set up multer to handle file uploads
 var storage = multer.diskStorage({
     destination: "./packages/file_handler/dist/uploads", // specify the destination folder
     filename: function (req, file, cb) {
-        cb(null, file.originalname); // use the original file name
+        var ext = path.extname(file.originalname);
+        var replacedString = replaceNonAlphabeticCharactersWithUnderscore(file.originalname);
+        cb(null, "".concat(replacedString).concat(ext)); // use the original file name
     },
 });
 var upload = multer({ storage: storage });
 // Serve files from the public directory
 // Set up a route handler to handle file uploads
 app.post("/file/upload", upload.single("file"), function (req, res) {
-    res.send("File uploaded successfully");
+    console.log(console.log(req.file));
+    res.json({ filename: req.file.filename });
 });
 app.listen(9090, function () { return console.log("App listening on ".concat(9090)); });
--- packages/file_handler/index.ts
+++ packages/file_handler/index.ts
@@ -3,16 +3,24 @@ const express = require("express");
 const multer = require("multer");
 import config from "config";
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.static("./packages/file_handler/dist/uploads"));
 
+function replaceNonAlphabeticCharactersWithUnderscore(str) {
+  // Replace spaces, hyphens, and any non-alphabetic characters with underscores
+  return str.replace(/[^a-zA-Z]/g, "_");
+}
 // Set up multer to handle file uploads
 const storage = multer.diskStorage({
   destination: "./packages/file_handler/dist/uploads", // specify the destination folder
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // use the original file name
+    let ext = path.extname(file.originalname);
+    let replacedString = replaceNonAlphabeticCharactersWithUnderscore(
+      file.originalname
+    );
+    cb(null, `${replacedString}${ext}`); // use the original file name
   },
 });
 const upload = multer({ storage: storage });
@@ -21,7 +29,8 @@ const upload = multer({ storage: storage });
 
 // Set up a route handler to handle file uploads
 app.post("/file/upload", upload.single("file"), (req, res) => {
-  res.send("File uploaded successfully");
+  console.log(console.log(req.file));
+  res.json({ filename: req.file.filename });
 });
 
 app.listen(9090, () => console.log(`App listening on ${9090}`));
